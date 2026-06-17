9a1a46ef4a337e57fb41ce2af868e78aa5616cac
Hugo Alves <hugocodeforall@gmail.com>
2026-06-17 11:03:28 +0100

    fix: correct Instagram link in social media profiles for accuracy

--- src/lib/social.ts
+++ src/lib/social.ts
@@ -3,6 +3,6 @@
  * Consumed by Footer.astro (icon links) and BaseSEO.astro (Organization sameAs).
  */
 export const SOCIAL_LINKS = {
-  instagram: 'https://www.instagram.com/waytosantiagoguide/',
+  instagram: 'https://www.instagram.com/thewaytosantiagoguide/',
   facebook: 'https://www.facebook.com/profile.php?id=61590837029060',
 } as const;
